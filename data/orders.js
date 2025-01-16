23c1cf1c6628571d7f9005428f7728b9f056b898
ShahidMZ <shahidmzubair@gmail.com>
2025-01-16 21:51:03 +0530

    Initial commit

---
+++ data/orders.js
@@ -0,0 +1,20 @@
+const orders = [
+    {
+        productName: "Product 1",
+        productNumber: 1,
+        paymentStatus: "Paid",
+        status: "Active"
+    },
+    {
+        productName: "Product 2",
+        productNumber: 2,
+        paymentStatus: "Refunded",
+        status: "Declined"
+    },
+    {
+        productName: "Product 3",
+        productNumber: 3,
+        paymentStatus: "Due",
+        status: "Pending"
+    }
+];
